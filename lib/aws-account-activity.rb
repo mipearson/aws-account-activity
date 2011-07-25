6bbc926d0f039ec25344f6360bcee610be2fca30
Michael Pearson <mipearson@gmail.com>
2011-07-25 17:14:36 +1000

    initial commit

---
+++ lib/aws-account-activity.rb
@@ -0,0 +1,7 @@
+module Aws
+  module Account
+    module Activity
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/aws-account-activity/version.rb
@@ -0,0 +1,7 @@
+module Aws
+  module Account
+    module Activity
+      VERSION = "0.0.1"
+    end
+  end
+end
